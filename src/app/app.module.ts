b7c97e890565074d8e578b5796ca74b635071eee
derickfelix <derickfelix4321@gmail.com>
2017-09-22 12:58:50 -0400

    Add profiles page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { ProfilesPage } from '../pages/profiles/profiles';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -15,19 +16,20 @@ import { GithubServiceProvider } from '../providers/github-service/github-servic
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    ProfilesPage,
     ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    ProfilesPage,
     ContactPage,
     HomePage,
     TabsPage
--- src/pages/profiles/profiles.ts
+++ src/pages/profiles/profiles.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { GithubServiceProvider } from '../../providers/github-service/github-service';
 /**
  * Generated class for the ProfilesPage page.
  *
@@ -15,11 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  profiles: any;
+  repos: any;
+  github_user = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private githubService: GithubServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilesPage');
   }
 
+  // On submit method
+  onSubmit() {
+    this.getProfile(this.github_user);
+    this.github_user = '';
+  }
+
+  reset() {
+    this.profiles = '';
+    this.github_user = '';
+  }
+  getProfile(username) {
+    this.githubService.getProfile(username).subscribe(response => {
+      this.profiles = response;
+      console.log(this.profiles);
+    });
+  }
 }
